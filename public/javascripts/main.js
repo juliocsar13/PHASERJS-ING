253459fbdcf0d41ef6304b41314c82a5ad0000e5
JULIO <julcsar13@gmail.com>
2016-12-09 12:26:25 -0500

    agdg

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -86,9 +86,10 @@ function signIn(){
     $.post('/login' , data)
         .done(function(result){
             console.log("HOLA MUNDO");
-            return window.location.pathname = '/entretenimiento'
+            //return window.location.pathname = '/entretenimiento'
+            console.log(window.location.pathname);
         })
-        //return window.location.pathname = '/entrete'
+        return window.location.pathname = '/entretenimiento'
 /*
     $.ajax({
         type         : 'POST',
